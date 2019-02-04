46ea6ebed7c21167e5da7f3deff38826bbca1c6c
Magnus Hovland Hoff <maghoff@gmail.com>
2019-02-04 08:28:47 +0100

    Add comment about safety of unsafe section

--- src/entry.rs
+++ src/entry.rs
@@ -7,7 +7,14 @@ pub struct Entry<'a> {
 
 impl<'a> Entry<'a> {
     pub fn name(&self) -> Result<&str, Error> {
-        let buf: &'a [u8; 8] = unsafe { std::mem::transmute(&self.id) };
+        let buf: &'a [u8; 8] = unsafe {
+            // I believe this is safe because the target type does not
+            // have any particular alignment requirements
+
+            // FIXME However, this is incorrect on big-endian machines
+
+            std::mem::transmute(&self.id)
+        };
         let name = buf.split(|&x| x == 0).next().ok_or(Error::InvalidEntry)?;
         verify!(name.len() > 0, Error::InvalidEntry);
         verify!(name.iter().all(|&x| x & 0x80 == 0), Error::InvalidEntry); // ASCII
